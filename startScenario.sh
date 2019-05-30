22e4b8b54b12e3c2f661912c8da3502bf472e7ca
damascenodiego <damascenodiego@usp.br>
2019-05-30 22:57:42 +0100

    bash script to start/kill scenario+server

---
+++ startScenario.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+run_python=/home/driverleics/opt/python3.6-venv/bin/python
+
+scenario=Town03GasStation
+town=Town03;
+
+while [ "$1" != "" ]; do
+    case $1 in
+    	town01Restaurant) 
+			cenario=Town01Restaurant
+			town=Town01
+			;;
+        town03GasStation)
+        	scenario=Town03GasStation
+        	town=Town03
+        	;;
+        *)
+			scenario=Town03GasStation
+			town=Town03
+			;;
+    esac
+    shift
+done
+
+nohup /home/driverleics/Downloads/carla/CARLA_0.9.5/CarlaUE4.sh /Game/Carla/Maps/$town > log.txt &
+sleep 7s
+
+$run_python /home/driverleics/git/scenario_runner-v095/scenario_runner.py --scenario $scenario &
+
+sleep 2s
+nohup $run_python /home/driverleics/git/scenario_runner-v095/manual_control_steeringwheel.py -v --res=1920x1080 > log_manual.txt
+
+killall -9 CarlaUE4 CarlaUE4.sh
+killall -9 CarlaUE4 CarlaUE4.sh
