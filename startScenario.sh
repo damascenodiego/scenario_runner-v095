b33e3d7e5b9fecd5c0bb3704fc2ceefee74d201c
damascenodiego <damascenodiego@usp.br>
2019-06-13 18:31:06 +0100

    startScenario minimizes server

--- startScenario.sh
+++ startScenario.sh
@@ -26,10 +26,14 @@ esac
 
 /home/driverleics/Downloads/carla/CARLA_0.9.5/CarlaUE4.sh /Game/Carla/Maps/$town > log_server.txt & 
 
-$run_python /home/driverleics/git/scenario_runner-v095/scenario_runner.py --scenario $scenario > log_scenario.txt &
+xdotool windowminimize `wmctrl -l | grep "CarlaUE4 (64-bit Development GLSL_430)"|cut -d\  -f1`
 
-sleep 7s
-$run_python /home/driverleics/git/scenario_runner-v095/manual_control_steeringwheel.py --res=1280x720 --fullscreen > log_client.txt
+$run_python /home/driverleics/git/scenario_runner-v095/scenario_runner.py --scenario $scenario --res=1280x720 --fullscreen > log_scenario.txt
+
+# $run_python /home/driverleics/git/scenario_runner-v095/scenario_runner.py --scenario $scenario > log_scenario.txt &
+
+# sleep 7s
+# $run_python /home/driverleics/git/scenario_runner-v095/manual_control_steeringwheel.py --res=1280x720 --fullscreen > log_client.txt
 
 
-killall -9 CarlaUE4 CarlaUE4.sh scenario_runner.py manual_control_steeringwheel.py 
+killall -9 CarlaUE4 CarlaUE4.sh
