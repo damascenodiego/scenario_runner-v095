4971962e91d35c221adc8db287efab536e996ff5
damascenodiego <damascenodiego@usp.br>
2019-06-28 16:35:09 +0100

    using $home variable in the bash script

--- startScenario.sh
+++ startScenario.sh
@@ -4,13 +4,13 @@
 ( killall -9 CarlaUE4 CarlaUE4.sh 2>&1 ) > /dev/null
 clear
 
-run_python=/home/driverleics/opt/python3.6-venv/bin/python
+run_python=$HOME/opt/python3.6-venv/bin/python
 
 scenario=Town03GasStation
 town=Town03;
 
 case $1 in
-	Town01Restaurant) 
+	Town01Restaurant)
 		scenario=Town01Restaurant
 		town=Town01
 		;;
@@ -37,23 +37,23 @@ case $1 in
 esac
 
 
-/home/driverleics/Downloads/carla/CARLA_0.9.5/CarlaUE4.sh /Game/Carla/Maps/$town > log_server.txt & 
+$HOME/Downloads/carla/CARLA_0.9.5/CarlaUE4.sh /Game/Carla/Maps/$town > log_server.txt &
 
 sleep 2s
 xdotool windowminimize `wmctrl -l | grep "CarlaUE4"|cut -d\  -f1`
 
-$run_python /home/driverleics/git/scenario_runner-v095/scenario_runner.py --scenario $scenario --res=1280x720 --fullscreen > log_scenario_ui.txt
+$run_python $HOME/git/scenario_runner-v095/scenario_runner.py --scenario $scenario --res=1280x720 --fullscreen > log_scenario_ui.txt
 
-# $run_python /home/driverleics/git/scenario_runner-v095/scenario_runner.py --scenario $scenario > log_scenario.txt &
+# $run_python $HOME/git/scenario_runner-v095/scenario_runner.py --scenario $scenario > log_scenario.txt &
 
 # sleep 7s
-# $run_python /home/driverleics/git/scenario_runner-v095/manual_control_steeringwheel.py --res=1280x720 --fullscreen > log_client.txt
+# $run_python $HOME/git/scenario_runner-v095/manual_control_steeringwheel.py --res=1280x720 --fullscreen > log_client.txt
 
-#cat /home/driverleics/git/scenario_runner-v095/score.csv  >  /home/driverleics/git/driverleics.github.io/_data/score.csv &
-#mv /home/driverleics/git/scenario_runner-v095/snapshots/* /home/driverleics/git/driverleics.github.io/activities/snapshots/
-#cd /home/driverleics/git/driverleics.github.io/
-#rm -rf /home/driverleics/git/driverleics.github.io/_site/
-#/home/driverleics/.rvm/rubies/default/bin/bundle exec jekyll pagemaster scores
+#cat $HOME/git/scenario_runner-v095/score.csv  >  $HOME/git/driverleics.github.io/_data/score.csv &
+#mv $HOME/git/scenario_runner-v095/snapshots/* $HOME/git/driverleics.github.io/activities/snapshots/
+#cd $HOME/git/driverleics.github.io/
+#rm -rf $HOME/git/driverleics.github.io/_site/
+#$HOME/.rvm/rubies/default/bin/bundle exec jekyll pagemaster scores
 
 #killall -9 CarlaUE4 CarlaUE4.sh || echo "CarlaUE4 was not running."
 ( killall -9 CarlaUE4 CarlaUE4.sh 2>&1 ) > /dev/null
